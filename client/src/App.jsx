f59030740a9fa7b04d286ba3e51935e6ffc976fe
fev <feventolla99@gmail.com>
2022-12-13 23:41:26 +0300

    styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div>
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<UserList />} />
--- client/src/component/user.jsx
+++ client/src/component/user.jsx
@@ -4,9 +4,58 @@ import { useDispatch, useSelector } from "react-redux";
 import React from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "../App.css";
+import plan3 from "../assets/plan1.svg";
+import "./userList.css";
+import { Heading, Button, Box } from "rebass";
+import styled from "@emotion/styled";
 
 const userList = () => {
+  const Button = styled.button`
+    padding: 12px;
+    background-color: black;
+    font-size: 24px;
+    border-radius: 4px;
+    color: white;
+    font-weight: bold;
+    &:hover {
+      color: #040d4c;
+      background-color: white;
+    }
+  `;
+
+  const Button1 = styled.button`
+    padding: 17px;
+    background-color: blue;
+    font-size: 15px;
+    border-radius: 6px;
+    color: white;
+    font-weight: bold;
+    border: 1px solid white;
+    margin-left: 10em;
+    margin-top: 2em;
+    &:hover {
+      color: #040d4c;
+      background-color: white;
+      border: 2px solid blue;
+    }
+  `;
+
+  const Button2 = styled.button`
+    padding: 17px;
+    background-color: #f50910;
+    font-size: 15px;
+    border-radius: 6px;
+    color: white;
+    font-weight: bold;
+    border: 1px solid white;
+    margin-left: 1em;
+    margin-top: 2em;
+    &:hover {
+      color: #040d4c;
+      background-color: white;
+      border: 2px solid red;
+    }
+  `;
   useEffect(() => {
     dispatch(getall());
   }, [getall]);
@@ -15,35 +64,64 @@ const userList = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   return (
-    <div className="app">
-      <div className="app-inside">
-        <h2>USER LIST</h2>
-        <button onClick={() => navigate("/register")}>Add user</button>
+    <div className="main">
+      <div className="main-inside">
+        <Heading fontSize={[5, 6, 7]} color="black">
+          User List
+        </Heading>
+        <div style={{ marginRight: 500, color: "#9E9D9D" }}>
+          A List of user that have been register in the system. A New user can
+          be added and the existing users can be updated or deleted...
+        </div>
+
+        <Button
+          style={{ marginLeft: 550 }}
+          onClick={() => navigate("/register")}
+        >
+          Add user
+        </Button>
         <div>
           {user.length > 0 ? (
             <div>
               {user.map((users, index) => (
                 <div key={index}>
-                  <h3>
-                    {users.firstname}
-                    <button
-                      className="btn"
-                      onClick={() =>
-                        navigate("/updateuser/" + users._id, {
-                          state: { id: users._id },
-                        })
-                      }
-                    >
-                      update
-                    </button>{" "}
-                    <button
-                      className="btn"
-                      text="delete"
-                      onClick={() => dispatch(deleteuser(users._id))}
-                    >
-                      Delete
-                    </button>{" "}
-                  </h3>
+                  <Box
+                    p={5}
+                    fontSize={3}
+                    width={[1, 1, 0.76]}
+                    color="black"
+                    bg="#EBECF3"
+                    m={4}
+                    sx={{
+                      border: "1px solid black",
+                      borderRadius: "20px",
+                    }}
+                  >
+                    <img src={plan3} alt="image" width={120} height={120} />
+                    <h3>
+                      Name: {users.firstname} {users.lastname}
+                      <div>
+                        Gender: {users.gender}
+                        <Button1
+                          className="btn"
+                          onClick={() =>
+                            navigate("/updateuser/" + users._id, {
+                              state: { id: users._id },
+                            })
+                          }
+                        >
+                          update
+                        </Button1>{" "}
+                        <Button2
+                          className="btn"
+                          text="delete"
+                          onClick={() => dispatch(deleteuser(users._id))}
+                        >
+                          Delete
+                        </Button2>{" "}
+                      </div>
+                    </h3>
+                  </Box>
                 </div>
               ))}
             </div>
